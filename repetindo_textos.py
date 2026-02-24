b2835e869d4e7114747726090edf280f08b06e9f
sllnoliveira <suellen.oliveira94@outlook.com>
2026-02-24 11:15:14 -0300

    Adiciona exercício 2 - Repetindo Textos

---
+++ repetindo_textos.py
@@ -0,0 +1,11 @@
+# Solicitando uma string ao usuário
+texto = input("Digite um texto: ")
+
+# Solicitando um número inteiro
+numero = int(input("Digite um número inteiro: "))
+
+# Repetindo o texto
+resultado = texto * numero
+
+# Exibindo o resultado
+print("O resultado é:", resultado)
