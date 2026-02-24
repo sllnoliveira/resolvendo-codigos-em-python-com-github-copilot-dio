fd997da9488591b4ceafe13af6842e662157dc74
sllnoliveira <suellen.oliveira94@outlook.com>
2026-02-24 11:20:31 -0300

    Adiciona exercício 3 - Operações Matemáticas Simples

---
+++ operacoes_matematicas.py
@@ -0,0 +1,9 @@
+# Solicitando dois números ao usuário
+numero1 = float(input("Digite o primeiro número: "))
+numero2 = float(input("Digite o segundo número: "))
+
+# Realizando a Soma
+Resultado = numero1 + numero2
+
+# Exibindo o resultado 
+print("Resultado da soma:", Resultado)
