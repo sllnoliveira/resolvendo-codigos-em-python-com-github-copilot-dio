9037a43d5423d0648568e00381860b83aa7bd21d
sllnoliveira <suellen.oliveira94@outlook.com>
2026-02-24 11:08:38 -0300

    Adiciona exercício 1 - Concatenando Dados

---
+++ concatenando_dados.py
@@ -0,0 +1,10 @@
+# solicitando dois dados ao usuário
+dado1 = input("Digite o primeiro dado: ")
+dado2 = input("Digite o segundo dado: ")
+
+# concatenando os dados
+resultado = dado1 + dado2
+
+# exibindo o resultado
+print("O resultado da concatenação é:", resultado)
+
