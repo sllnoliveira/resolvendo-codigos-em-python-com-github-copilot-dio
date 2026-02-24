061b4ee1f33bba463ffc262640af8d99617a009c
sllnoliveira <suellen.oliveira94@outlook.com>
2026-02-24 11:35:09 -0300

    Adicona exercício 4 - Verificando números pares e ímpares

---
+++ par_ou_impar.py
@@ -0,0 +1,8 @@
+# Solicitando um número inteiro ao usuário
+numero = int(input("Digite um número inteiro: "))
+
+# Verificando se é par ou ímpar
+if numero % 2 == 0:
+    print("O número é PAR.")
+else:
+    print("O número é ÍMPAR.")
