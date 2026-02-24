a507394619d40e8ee88b2a7dd9001feb424419ef
sllnoliveira <suellen.oliveira94@outlook.com>
2026-02-24 11:39:36 -0300

    Adiciona exercício 5 - Média de notas

---
+++ media_notas.py
@@ -0,0 +1,11 @@
+# Solicitando três notas ao usuário
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+nota3 = float(input("Digite a terceira nota: "))
+
+# Calculando a média
+media = (nota1 + nota2 + nota3) / 3
+
+# Exibindo o resultado
+print("A média das notas é:", media)
+
